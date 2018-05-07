5592340579dc8fccb91315af17900ed16ce3c583
Florian Schrag <florian@schrag.org>
2018-05-07 10:00:39 +0200

    Update main.go

--- main.go
+++ main.go
@@ -109,7 +109,7 @@ func addKeyCookie(w http.ResponseWriter, k string) {
 		Value:    k,
 		MaxAge:   7 * 24 * 3600,
 		HttpOnly: true,
-		Path:     "/"
+		Path:     "/",
 	}
 	http.SetCookie(w, &c)
 }
